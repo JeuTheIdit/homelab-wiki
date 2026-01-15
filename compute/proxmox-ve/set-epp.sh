5f3da5fde50a6334ac21328885e04c3cdb57630f
JeuTheIdit <61256624+JeuTheIdit@users.noreply.github.com>
2026-01-14 19:56:39 -0600

    Update set-epp.sh

--- compute/proxmox-ve/set-epp.sh
+++ compute/proxmox-ve/set-epp.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-# Set every CPU to the "power" epp
+# set every CPUs epp
 
 for epp_file in /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference; do
-    # Only touch the file if it exists (CPU may be disabled)
+    # only touch the file if it exists (CPU may be disabled)
     if [[ -w "$epp_file" ]]; then
-        # Choose between: performance, balance_performance, balance_power, or power
+        # choose between: performance, balance_performance, balance_power, or power
         echo balance_power > "$epp_file"
     fi
 done
